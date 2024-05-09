2180829a8b4faf32d9f94ec02cdde643fe5e39c8
iskake <iskake@protonmail.com>
2024-05-09 20:48:20 +0200

    Update Show for Instruction

--- src/HS6502/Debug.hs
+++ src/HS6502/Debug.hs
@@ -10,17 +10,31 @@ data Instruction = Instruction
     , instArg :: Maybe (Either Word8 Word16)}
 
 instance Show Instruction where
-    show (Instruction inst aMode value) = show inst ++ " " ++ show aMode ++ " " ++ show (hex8Or16 <$> value)
+    show (Instruction inst aMode value) = show inst <> " " <> addrModeShow aMode value
+
+addrModeShow :: AddrMode -> Maybe (Either Word8 Word16) -> String
+addrModeShow Imm  (Just val) = "#$" <> hex8Or16 val
+addrModeShow ZP   (Just val) = "$" <> hex8Or16 val
+addrModeShow ZPX  (Just val) = "$" <> hex8Or16 val <> ",x"
+addrModeShow ZPY  (Just val) = "$" <> hex8Or16 val <> ",y"
+addrModeShow Rel  (Just val) = "$" <> hex8Or16 val
+addrModeShow Abs  (Just val) = "$" <> hex8Or16 val
+addrModeShow AbsX (Just val) = "$" <> hex8Or16 val <> ",x"
+addrModeShow AbsY (Just val) = "$" <> hex8Or16 val <> ",y"
+addrModeShow Ind  (Just val) = "[$" <> hex8Or16 val <> "]"
+addrModeShow IndX (Just val) = "[$" <> hex8Or16 val <> ",x]"
+addrModeShow IndY (Just val) = "[$" <> hex8Or16 val <> "],y"
+addrModeShow _ _ = ""
 
 hex8Or16 :: Either Word8 Word16 -> String
 hex8Or16 (Left  a) = hex8 a
 hex8Or16 (Right b) = hex16 b
 
 printCPUState :: Memory a => CPUState a -> String
-printCPUState (CPUState a x y p sp pc _) = "A:" ++ hex8 a ++ " X:" ++ hex8 x ++ " Y:" ++ hex8 y ++ " P:" ++ show p ++ " SP:" ++ hex8 sp ++ " PC:" ++ hex16 pc
+printCPUState (CPUState a x y p sp pc _) = "A:" <> hex8 a <> " X:" <> hex8 x <> " Y:" <> hex8 y <> " P:" <> show p <> " SP:" <> hex8 sp <> " PC:" <> hex16 pc
 
 printNextInstr :: Memory a => CPUState a -> String
-printNextInstr (CPUState _ _ _ _ _ pc mem) = "  ->" ++ hex16 pc ++ ": " ++ show (head (disasSect pc (pc+1) mem))
+printNextInstr (CPUState _ _ _ _ _ pc mem) = "  ->" <> hex16 pc <> ": " <> show (head (disasSect pc (pc+1) mem))
 
 disasOp :: Word8 -> (Inst, AddrMode, Word16 -> Maybe (Either Word8 Word16))
 disasOp op = (opToInst op, aMode, val)
@@ -32,7 +46,7 @@ disasOp op = (opToInst op, aMode, val)
                 _ -> const Nothing
 
 disasSect :: Memory a => Word16 -> Word16 -> a -> [Instruction]
-disasSect from to mem | from == to = []
+disasSect from to mem | from >= to = []
                       | otherwise = (Instruction i a (f next)) : disasSect (from + (1 + addrModeArgCount a)) to mem
                       where
                         op = readAddr mem from
